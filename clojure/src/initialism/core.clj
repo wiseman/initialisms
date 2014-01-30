a3174c63280ccad024f6826d46b3af994c5346e6
John Wiseman <jwiseman@morseproject.com>
2014-01-30 15:35:42 -0800

    Added --dump-hashes and --trigram-mode.

--- clojure/src/initialism/core.clj
+++ clojure/src/initialism/core.clj
@@ -12,6 +12,12 @@
 (gflags/define-boolean "dump-tokens"
   false
   "Dump a list of all tokens found in the corpora, then exit.")
+(gflags/define-boolean "dump-hashes"
+  false
+  "Dump a list of hash frequencies for tokens and states, then exit.")
+(gflags/define-boolean "trigram-mode"
+  false
+  "Show exact matches for trigrams")
 
 
 (defmacro log-time
@@ -75,6 +81,12 @@
     (get freqs key (/ 0.5 n))))
 
 
+(defn corpus-stats [tokens]
+  {:unigrams (frequencies tokens)
+   :bigrams (frequencies (n-grams 2 tokens))
+   :trigrams (frequencies (n-grams 3 tokens))})
+
+
 (defn read-corpus [sentencizer tokenizer corpus]
   (log/info "Loading corpus" corpus)
   (let [sentences (sentencizer (slurp corpus))
@@ -84,25 +96,25 @@
                                 (if (seq tokens)
                                   (concat ["$"] tokens)
                                   '())))
-                            sentences))
-        stats {:unigrams (frequencies tokens)
-               :bigrams (frequencies (n-grams 2 tokens))}]
-    (log/info "Finished analyzing corpus" corpus)
-    stats))
+                            sentences))]
+    tokens))
 
 
 (defn merge-corpus-stats [s1 s2]
   {:unigrams (merge-with + (s1 :unigrams) (s2 :unigrams))
-   :bigrams (merge-with + (s1 :bigrams) (s2 :bigrams))})
+   :bigrams (merge-with + (s1 :bigrams) (s2 :bigrams))
+   :trigrams (merge-with + (s1 :trigrams) (s2 :trigrams))})
 
 
-(defn read-corpora [corpora]
+(defn read-corpora [corpora processor]
   (let [sentencizer (get-sentence-detector)
         tokenizer (get-tokenizer)
         stats (log-time (str "Loading " (count corpora) " corpora")
                 (let [stats (reduce
                              merge-corpus-stats
-                             (pmap (partial read-corpus sentencizer tokenizer)
+                             (pmap (comp
+                                    processor
+                                    (partial read-corpus sentencizer tokenizer))
                                    corpora))]
                   (assoc
                       stats
@@ -140,10 +152,45 @@
     (recur (read-line))))
 
 
+(defn zip [& colls]
+  (apply (partial map vector) colls))
+
+
+(defn trigram-repl [stats]
+  (let [trigram-freqs (:trigrams stats)
+        initials-words-map
+        (reduce (fn [initials-map [trigram _]]
+                  (let [initials (string/join (nth (apply zip trigram) 0))]
+                    (assoc
+                        initials-map initials
+                        (conj (get initials-map initials [])
+                              [(trigram-freqs trigram) (vec trigram)]))))
+                {}
+                trigram-freqs)]
+    (log/info (count trigram-freqs) "distinct trigrams")
+    (loop [line (read-line)]
+      (when-not line
+        (System/exit 0))
+      (let [line (string/lower-case line)]
+        (log/info "Decoding" line)
+        (doseq [[count trigram] (reverse (sort (initials-words-map line)))]
+          (println count (string/join " " trigram)))
+        (recur (read-line))))))
+
+
 (defn dump-tokens [stats]
   (log/info "Dumping tokens")
   (doseq [[token count] (sort (stats :unigrams))]
     (println token)))
+
+
+(defn dump-hashes [stats]
+  (println "# unigrams")
+  (doseq [[hash count] (frequencies (map hash (:unigrams stats)))]
+    (println hash count))
+  (println "# bigrams")
+  (doseq [[hash count] (frequencies (map hash (:bigrams stats)))]
+    (println hash count)))
 
 
 (defn -main
@@ -151,8 +198,10 @@
   [& args]
   (let [unparsed-args (gflags/parse-flags (concat [nil] args))
         flags (gflags/flags)
-        stats (read-corpora unparsed-args)]
-    (if (flags :dump-tokens)
-      (dump-tokens stats)
-      (repl stats)))
+        stats (read-corpora unparsed-args corpus-stats)]
+    (cond
+     (flags :dump-tokens) (dump-tokens stats)
+     (flags :dump-hashes) (dump-hashes stats)
+     (flags :trigram-mode) (trigram-repl stats)
+     :else (repl stats)))
   (System/exit 0))
