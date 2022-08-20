f9c80bd7f65e1bd0c14aa214a8faf0bf85784f14
the-REAL-beautysleep <mark.callaghan@susbuddy.com>
2022-08-20 20:46:18 +0100

    finishing up for the night. SSL must be added for this to work on the website

--- decoder.py
+++ decoder.py
@@ -1,4 +1,4 @@
-from itertools import permutations
+from itertools import permutations, zip_longest
 import logging
 import os.path
 import re
@@ -6,6 +6,7 @@ import time
 import gflags
 import nltk
 import nltk.tokenize
+import concurrent.futures
 
 FLAGS = gflags.FLAGS
 logger = logging.getLogger(__name__)
@@ -77,7 +78,7 @@ class Decoder(object):
         self.results_list = []
         self.ordered = ordered
         extra_corpora = nltk.corpus.PlaintextCorpusReader(CORPUS_ROOT, '.*')
-        corpus = extra_corpora.raw("words.txt")
+        corpus = extra_corpora.raw("_all.txt")
         print("this is the corpus (uncomment)")
         # print(corpus)
         self.corpus = corpus
@@ -87,7 +88,7 @@ class Decoder(object):
         temp = ch[i]
         ch[i] = ch[j]
         ch[j] = temp
-    
+
     # Recursive function to generate all permutations of a string
     def permutations(self, ch, curr_index=0):
 
@@ -104,9 +105,9 @@ class Decoder(object):
 
         seperator = "[\\s:;,–]*"
         rest_of_word = "[a-zA-Z0-9]+"
-        customer_regex = "\\b"
+        customer_regex = ""
         for letter in initials:
-            temp_regex = letter + rest_of_word + seperator
+            temp_regex = "\\b" + letter + rest_of_word + seperator
             customer_regex += temp_regex
         customer_regex = customer_regex[:-len(seperator)]
         # print("customer_regex", customer_regex)
@@ -115,25 +116,61 @@ class Decoder(object):
 
     # flattens array
     # drops all empty elements
-    # TODO drop elements not the same length as the initials string
+    # removes duplicates
     def tidyResultsList(self):
         self.results_list = [item for sublist in self.results_list for item in sublist]
         self.results_list = [x for x in self.results_list if x]
+        self.results_list = list(dict.fromkeys(self.results_list))
+
+    # creates regex from Initials
+    # searches corpas and appends results to results_list
+    def createFindAppend(self, initials):
+        try:
+            regex = self.createRegex(initials)   
+            self.results_list.append(regex.findall(self.corpus))
+
+        except Exception as ex:
+            print("error", ex)
+
+    def groupedCreateFindAppend(self, group_of_intials_permutations):
+        try:
+            for i in group_of_intials_permutations:
+                self.createFindAppend(i)
+        except Exception as ex:
+            print("error", ex)
 
     def decode(self, initials):
         timer = Timer()
         print("self.ordered =", self.ordered)
         if not self.ordered:
             all_permutations_of_initials = permutations(list(initials))
-            for permutation in all_permutations_of_initials:
-                print("permutation", permutation)
-                regex = self.createRegex(permutation)
-                self.results_list.append(regex.findall(self.corpus))
+            self.groupedCreateFindAppend(all_permutations_of_initials)
+
+            # TODO consider ways to speed up code 
+            # https://stackoverflow.com/questions/15143837/how-to-multi-thread-an-operation-within-a-loop-in-python
+            # executor = concurrent.futures.ProcessPoolExecutor(8)
+            # futures = [executor.submit(self.createFindAppend, permutation) for permutation in all_permutations_of_initials]
+            # concurrent.futures.wait(futures)
         else:
-            regex = self.createRegex(initials)
-            self.results_list.append(regex.findall(self.corpus))
+            self.createFindAppend(initials)
 
         self.tidyResultsList()
 
         logger.info('Decoding %r took %s s', initials, timer.elapsed())
         return self.results_list
+
+
+def grouper(iterable, n, *, incomplete='fill', fillvalue=None):
+    "Collect data into non-overlapping fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, fillvalue='x') --> ABC DEF Gxx
+    # grouper('ABCDEFG', 3, incomplete='strict') --> ABC DEF ValueError
+    # grouper('ABCDEFG', 3, incomplete='ignore') --> ABC DEF
+    args = [iter(iterable)] * n
+    if incomplete == 'fill':
+        return zip_longest(*args, fillvalue=fillvalue)
+    if incomplete == 'strict':
+        return zip(*args, strict=True)
+    if incomplete == 'ignore':
+        return zip(*args)
+    else:
+        raise ValueError('Expected fill, strict, or ignore')
